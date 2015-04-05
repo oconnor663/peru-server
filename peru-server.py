3f1e2a62c4853c15075142abb641aa6655b68f33
Jack O'Connor <oconnor663@gmail.com>
2015-04-04 22:21:07 -0400

    use sqlite3, rename to peru-server.py

--- peru-server.py
+++ peru-server.py
@@ -1,14 +1,30 @@
 #! /usr/bin/env python
 
+import contextlib
 import html
+import os
+import sqlite3
+
 import flask
 
 app = flask.Flask(__name__)
 
-blobs = []
+DB_PATH = './peru-server.sqlite3'
+
+
+@contextlib.contextmanager
+def db_connection():
+    if not os.path.exists(DB_PATH):
+        with sqlite3.connect(DB_PATH) as conn:
+            conn.execute('CREATE TABLE blobs (id INTEGER PRIMARY KEY, blob)')
+    with sqlite3.connect(DB_PATH) as conn:
+        yield conn
 
 
 def get_blobs():
+    with db_connection() as conn:
+        rows = conn.execute('SELECT blob FROM blobs ORDER BY id').fetchall()
+    blobs = [row[0] for row in rows]
     return ''.join('''
         <p style='white-space: pre-wrap; font-family: monospace'>{}</p>
         <form action="delete/{}" method="POST">
@@ -34,13 +50,16 @@ def handle_index():
 
 @app.route('/submit', methods=['POST'])
 def handle_submit():
-    blobs.append(flask.request.values['blob'])
+    blob = flask.request.values['blob']
+    with db_connection() as conn:
+        conn.execute('INSERT INTO blobs (blob) VALUES (?)', [blob])
     return flask.redirect('/')
 
 
 @app.route('/delete/<id>', methods=['POST'])
 def handle_delete(id=None):
-    blobs.pop(int(id))
+    with db_connection() as conn:
+        conn.execute('DELETE FROM blobs WHERE id = ?', [id])
     return flask.redirect('/')
 
 
