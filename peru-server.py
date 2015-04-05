a4d803437d6883ae2e65f5f92ec307a2f52221c0
Jack O'Connor <oconnor663@gmail.com>
2015-04-05 01:18:40 -0400

    generate the peru.yaml file

--- peru-server.py
+++ peru-server.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import collections
 import contextlib
 import html
 import os
@@ -25,12 +26,19 @@ def get_blobs():
     with db_connection() as conn:
         rows = conn.execute(
             'SELECT id, blob FROM blobs ORDER BY id').fetchall()
+    blobs = collections.OrderedDict()
+    for id, blob in rows:
+        blobs[id] = blob
+    return blobs
+
+
+def get_blob_markup():
     return ''.join('''
         <p style='white-space: pre-wrap; font-family: monospace'>{}</p>
         <form action="delete/{}" method="POST">
             <input type="submit" value="Delete">
         </form>
-    '''.format(html.escape(blob), i) for i, blob in rows)
+    '''.format(html.escape(blob), i) for i, blob in get_blobs().items())
 
 
 @app.route('/')
@@ -38,14 +46,20 @@ def handle_index():
     return '''
 <html>
     <body>
+        <h1>Peru Server</h1>
+        <p>To use these module definitions, add <a href='{url}'>{url}</a> to
+        your own project file as a <strong>curl</strong> module.</p>
+        <h2>Add a new module</h2>
         <form action="submit" method="POST">
             <textarea name="blob" rows="5" cols="80"></textarea><br>
             <input type="submit" value="Submit">
         </form>
-{}
+        <h2>Existing modules</h2>
+{modules}
     </body>
 </html>
-'''.format(get_blobs())
+'''.format(url=flask.url_for('handle_peru_yaml', _external=True),
+           modules=get_blob_markup())
 
 
 @app.route('/submit', methods=['POST'])
@@ -61,6 +75,13 @@ def handle_delete(id=None):
     with db_connection() as conn:
         conn.execute('DELETE FROM blobs WHERE id = ?', [id])
     return flask.redirect('/')
+
+
+@app.route('/peru.yaml')
+def handle_peru_yaml():
+    return flask.Response(
+        '\n'.join(get_blobs().values()),
+        mimetype='text/plain')
 
 
 if __name__ == '__main__':
